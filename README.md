6f3b514de0c294c6b5db2674bedbc0a230eebf65
Danny White <hi@dannywhite.site>
2021-01-29 19:44:35 +1100

    feat: footer, following list

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Waterhole
 
-Just an experiment. If it feels right, I'll mention it to the fine folks at iNaturalist.
+iNaturalist is missing a friendly feed on the iOS app and a mobile-layout for its [Following webpage](https://www.inaturalist.org/home?tab=following). Waterhole is an attempt to fill that gap, although just for me; it currently (manually) follows the users that I follow.
+
+Waterhole is just an experiment. If it feels right, I'll mention it to the fine folks at iNaturalist and see if they have similar ideas and/or if I can help.
 
 ## Usage
 
--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const loader = document.querySelector("#loader");
 const intro = document.querySelector("#intro");
 const feed = document.querySelector("#feed");
-const usersInFeed = "dannywhite%2Clexilombari%2Cnicole-gonzalez"
+const users = document.querySelector("#users");
 
+const followingList = ["dannywhite", "lexilombari", "nicole-gonzalez"];
+// Show following list on page
+followingList.map(item => {
+    const listItem = document.createElement("li");
+    listItem.textContent = item;
+    users.appendChild(listItem)
+})
+
+// Get iNaturalist data
 async function getData() {
-    const url = `https://api.inaturalist.org/v1/observations?user_login=${usersInFeed}&order=desc&order_by=created_at`;
+    const url = `https://api.inaturalist.org/v1/observations?user_login=${followingList.join("%2C")}&order=desc&order_by=created_at`;
     const response = await fetch(url);
     const data = await response.json();
     const feedData = data.results;
@@ -15,11 +24,10 @@ async function getData() {
 
     // Map through each item
     feedData.map(item => {
-        // console.log(item);
+        console.log(item);
 
         // Define elements
         const feedItem = document.createElement("li");
-        
         const feedItemImgLink = document.createElement("a");
         const feedItemTitle = document.createElement("h2");
         const feedItemAuthor = document.createElement("p");
@@ -34,21 +42,32 @@ async function getData() {
         feedItemTitle.appendChild(feedItemTextLink)
         // Add attributes to links
         const feedItemLinks = [feedItemImgLink, feedItemTextLink]
-        feedItemLinks.map(link => link.setAttribute('href', item.uri));
-        feedItemLinks.map(link => link.setAttribute('target', "_blank"));
+        feedItemLinks.map(link => {
+            link.setAttribute('href', item.uri);
+            link.setAttribute('target', "_blank");
+            link.setAttribute('rel', "noopener noreferrer");
+        });
         
 
         // Add data to elements
         feedItemImg.setAttribute('src', `https://static.inaturalist.org/photos/${item.photos[0].id}/medium.jpg`);
+        
         if (item.taxon.preferred_common_name) {
             feedItemTitle.textContent = item.taxon.preferred_common_name;
+            feedItemImg.setAttribute("title", item.taxon.preferred_common_name)
+            feedItemImg.setAttribute("alt", `A photo of a ${item.taxon.preferred_common_name}`)
         } else if (item.taxon.name) {
             feedItemTitle.textContent = item.taxon.name;
+            feedItemImg.setAttribute("title", item.taxon.name)
+            feedItemImg.setAttribute("alt", `A photo of a ${item.taxon.name}`)
         } else if (item.species_guess) {
             feedItemTitle.textContent = item.species_guess;
+            feedItemImg.setAttribute("title", item.species_guess)
+            feedItemImg.setAttribute("alt", `A photo of a ${item.species_guess}`)
         } else {
             feedItemTitle.textContent = "Unknown";
         }
+        
         item.user.name ? feedItemAuthor.textContent = item.user.name : feedItemAuthor.textContent = item.user.login;
         
 
@@ -64,4 +83,5 @@ async function getData() {
     })
 }
 
+// Show iNaturalist data on page
 getData();
